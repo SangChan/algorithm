be139ca980300cf11e0abffbe89270eedcb9ed02
SangChan Lee <sangchan82@gmail.com>
2015-04-24 17:00:51 +0900

    add print method for linked list

--- linkedlist.c
+++ linkedlist.c
@@ -44,6 +44,35 @@ int delete_node()
     return r;
 }
 
+void print_list(node_t *from)
+{
+    node_t *node = from;
+    while(node != NULL) {
+        printf("%d ", node->key);
+        node = node->next;
+    }
+}
+
+void print_list_recursive(node_t *from)
+{
+    if (from == NULL)
+    {
+        return ;
+    }
+    printf("%d ", from->key);
+    print_list_recursive(from->next);
+}
+
+void print_list_recursive_reverse(node_t *from)
+{
+    if (from == NULL)
+    {
+        return ;
+    }
+    print_list_recursive_reverse(from->next);
+    printf("%d ", from->key);
+}
+
 int main()
 {
     int number, r;
@@ -60,6 +89,14 @@ int main()
             printf("[%d]\n",r);
         }
     } while (number >= 0);
+
+    printf(">>iterator\n");
+    print_list(head);
+    printf("\n>>recursive\n");
+    print_list_recursive(head);
+    printf("\n>>reverse recursive\n");
+    print_list_recursive_reverse(head);
+    printf("\n>>the end\n");
     
     return 0;
 }
