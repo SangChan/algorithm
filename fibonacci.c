848368e44ffbca4fe6c48136300a1a618d90d1ee
SangChan Lee <sangchan82@gmail.com>
2015-05-12 17:26:10 +0900

    add fibo2 removing duplicate

--- fibonacci.c
+++ fibonacci.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+#define MAXN 200
+
+long long fibo2(int n)
+{
+	static long long memo[MAXN];
+	if (memo[n] > 0)
+	{
+		return memo[n];
+	}
+	if (n == 1|| n == 2)
+	{
+		return memo[n] = 1;
+	}
+	else 
+	{
+		return memo[n] = fibo2(n-1)+fibo2(n-2);
+	}
+}
+
 long long fibo(int n)
 {
 	if (n == 1 || n ==2)
@@ -17,6 +36,6 @@ int main()
 	int input_n;
 	printf("insert n : ");
 	scanf("%d",&input_n);
-	printf("%lld\n", fibo(input_n));
+	printf("%lld\n", fibo2(input_n));
 	return 0;
 }
