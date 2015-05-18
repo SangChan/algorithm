911c3138cce0c4d9a110203f7d179d15928f3f2e
SangChan Lee <sangchan82@gmail.com>
2015-05-18 17:09:00 +0900

    add dynamic programming

--- path_finder.c
+++ path_finder.c
@@ -4,6 +4,41 @@
 #define N 100
 
 int map[M][N];
+long long path[M][N];
+
+void calculate_path(int m, int n)
+{
+	int i,j;
+
+	path[0][0] = map[0][0];
+
+	for(i = 1; i < m; i++)
+	{
+		if(map[i][0] == 0)
+			path[i][0] = 0;
+		else
+			path[i][0] = path[i-1][0];
+	}
+
+	for(j = 1; j < n; j++)
+	{
+		if(map[0][j] == 0)
+			path[0][j] = 0;
+		else
+			path[0][j] = path[0][j-1];
+	}
+
+	for (i = 1; i < m; i++)
+	{
+		for (j = 1; j < n; j++)
+		{
+			if(map[i][j] == 0)
+				path[i][j] = 0;
+			else
+				path[i][j] = path[i-1][j] + path[i][j-1];
+		}
+	}
+}
 
 long long num_path(int m, int n)
 {
@@ -27,6 +62,7 @@ int main()
 			scanf("%d",&map[i][j]);
 		}
 	}
-	printf("%lld\n",num_path(m-1,n-1));
+	calculate_path(m,n);
+	printf("%lld\n",path[m-1][n-1]);
 	return 0;
 }
