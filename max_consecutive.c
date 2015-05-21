351ed61b362b82733f38ed4eed46dc00cfec1d4d
SangChan Lee <sangchan82@gmail.com>
2015-05-21 16:38:08 +0900

    add dynamic programming

--- max_consecutive.c
+++ max_consecutive.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 
+#define N 100
+
 int max(int x, int y)
 {
 	if(x > y)
 		return x;
 	return y;
 }
+
+int c[N];
+
+void calculate_max_consecutive_sum(int s[],int n)
+{
+	c[0] = s[0];
+	for (int i = 0; i < n; i++)
+	{
+		c[i] = max(s[i],s[i]+c[i-1]);
+	}
+}
+
+int find_max_consecutive_sum2(int s[], int n)
+{
+	int max_sum = 0;
+
+	for(int i = 0; i < n; i++)
+	{
+		if (c[i] > max_sum)
+		{
+			max_sum = c[i];
+		}
+		return max_sum;
+	}
+}
+
+int find_max_consecutive_sum(int s[], int n)
+{
+	if (n == 1)
+	{
+		return s[0];
+	}
+	return max(c[n-1], find_max_consecutive_sum(s,n-1));
+}
+
 
 int max_sum2(int s[], int n)
 {
