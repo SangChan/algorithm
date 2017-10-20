f1fa785df0eea081841ddd8ac1166c77bf6adc46
Sangchan Lee <sangchan.lee@ef.com>
2017-10-20 17:21:14 +0800

    add code

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -2,6 +2,35 @@
 
 import Foundation
 
-var str = "Hello, playground"
+var list = [1,2,3,5,8,2,3,4,5]
 
-//: [Next](@next)
+func sum(a: [Int], sum: Int) -> Bool {
+    var index = 0
+    for i in a {
+        for j in index ..< a.count {
+            if i + j == sum {
+                return true
+            }
+        }
+        index += 1
+    }
+    return false
+}
+
+sum(a: list, sum: 10)
+
+func sum2(a : [Int], sum: Int) -> Bool {
+    var dic : [Int : Int] = [:]
+    for i in a {
+        dic[i] = sum - i
+    }
+    
+    for i in dic.keys {
+        if let withA = dic[i], let _ = a.index(of: withA) {
+            return true
+        }
+    }
+    return false
+}
+
+sum2(a: list, sum: 10)
