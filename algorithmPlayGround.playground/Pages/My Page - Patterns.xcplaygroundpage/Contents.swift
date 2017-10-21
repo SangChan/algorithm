52d7215428f72879dde99893283b009c2bfefa10
SangChan Lee <sangchan82@gmail.com>
2017-10-21 20:22:19 +0800

    Strategy patterns

--- algorithmPlayGround.playground/Pages/My Page - Patterns.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - Patterns.xcplaygroundpage/Contents.swift
@@ -18,3 +18,44 @@ class Singleton {
 
 //let v = Singleton.sharedInstance
 //v.description()
+
+protocol Strategy {
+    func doOperation(_ a : Int, _ b : Int) -> Int
+}
+
+class OperationAdd : Strategy{
+    func doOperation(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
+
+class OperationSubstract : Strategy {
+    func doOperation(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+}
+
+class OperationMultipication : Strategy {
+    func doOperation(_ a: Int, _ b: Int) -> Int {
+        return a * b
+    }
+}
+
+class StrategyContext {
+    fileprivate let starategy : Strategy
+    
+    init(strategy : Strategy) {
+        self.starategy = strategy
+    }
+    
+    func excute(_ a: Int, _ b: Int) -> Int {
+        return self.starategy.doOperation(a, b)
+    }
+}
+
+let newContext = StrategyContext(strategy: OperationAdd())
+newContext.excute(1, 1)
+let newContext2 = StrategyContext(strategy: OperationMultipication())
+newContext2.excute(1, 1)
+let newContext3 = StrategyContext(strategy: OperationSubstract())
+newContext3.excute(1, 1)
