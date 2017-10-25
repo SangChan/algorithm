88adf0ee20dafcfaa33ab6bdbc37a33fe7ec6f5c
SangChan Lee <sangchan82@gmail.com>
2017-10-25 10:21:06 +0800

    add factory pattern

--- algorithmPlayGround.playground/Pages/My Page - Patterns.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - Patterns.xcplaygroundpage/Contents.swift
@@ -2,22 +2,26 @@
 
 import Foundation
 
+// Singleton Pattern
+
 class Singleton {
     static let sharedInstance = Singleton()
     fileprivate let hash : Int
     private init() {
         self.hash = Int(arc4random())
     }
-    func description() {
-        print("\(self.hash)")
+    func description() -> String{
+        return String(self.hash)
     }
 }
 
-//let s = Singleton.sharedInstance
-//s.description()
+let s = Singleton.sharedInstance
+s.description()
 
-//let v = Singleton.sharedInstance
-//v.description()
+let v = Singleton.sharedInstance
+v.description()
+
+// Strategy Pattern
 
 protocol Strategy {
     func doOperation(_ a : Int, _ b : Int) -> Int
@@ -59,3 +63,52 @@ let newContext2 = StrategyContext(strategy: OperationMultipication())
 newContext2.excute(1, 1)
 let newContext3 = StrategyContext(strategy: OperationSubstract())
 newContext3.excute(1, 1)
+
+// Factory Pattern
+
+protocol Pizza {
+    var topping : [String] { get set }
+    func bake()
+}
+
+enum PizzaType {
+    case italian, chicago, newyork
+}
+
+class ItailianPizza : Pizza {
+    var topping : [String] = ["thin douch", "cheese"]
+    func bake() {
+        print("Mamamia!")
+    }
+}
+
+class ChicagoPizza : Pizza {
+    var topping : [String] = ["big dough", "buch of cheese"]
+    func bake() {
+        print("this is chicago pizza!")
+    }
+}
+
+class NewyorkPizza : Pizza {
+    var topping : [String] = []
+    func bake() {
+        print("what is newyork pizza?")
+    }
+}
+
+class PizzaFactory {
+    static func createPizza(type: PizzaType) -> Pizza {
+        switch type {
+        case .italian:
+            return ItailianPizza()
+        case .chicago:
+            return ChicagoPizza()
+        case .newyork:
+            return NewyorkPizza()
+        }
+    }
+}
+
+let newPizza = PizzaFactory.createPizza(type: .newyork)
+newPizza.bake()
+
