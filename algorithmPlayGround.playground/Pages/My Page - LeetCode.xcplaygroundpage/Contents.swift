be6a9a9976f80b292c7d6686f75af1a16492187a
SangChan Lee <sangchan82@gmail.com>
2017-11-28 21:10:42 +0800

    add avltree for tree example

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -373,3 +373,59 @@ rmDpEx2.addLink(value: 3)
 rmDpEx2.deleteDuplicates()
 
 // 101. Symmetric Tree
+
+class AVLTree<T: Comparable> {
+    var key : T!
+    var left : AVLTree?
+    var right : AVLTree?
+    
+    lazy var height : Int = {
+        var nodeHeight = 0
+        nodeHeight = max(getNodeHeight(aNode: self.left), getNodeHeight(aNode: self.right)) + 1
+        return nodeHeight
+    }()
+    
+    func addNode(key: T) {
+        if self.key == nil {
+            self.key = key
+            return
+        }
+        
+        guard let selfKey = self.key else { return }
+        
+        if key < selfKey {
+            if self.left != nil {
+                left!.addNode(key: key)
+            } else {
+                let leftChild = AVLTree<T>()
+                leftChild.key = key
+                self.left = leftChild
+            }
+        } else if key > selfKey {
+            if self.right != nil {
+                right!.addNode(key: key)
+            } else {
+                let rightChild = AVLTree<T>()
+                rightChild.key = key
+                self.right = rightChild
+            }
+        }
+    }
+    
+    func getNodeHeight(aNode : AVLTree!) -> Int {
+        if aNode == nil {
+            return 0
+        }
+        return height
+    }
+    
+    func isTreeBalanced() -> Bool {
+        guard self.key != nil else { return false }
+        
+        if abs(getNodeHeight(aNode: self.left) - getNodeHeight(aNode: self.right)) <= 1 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
