9d6e8e3a7feeb4452a3dd9ba64b1745db6825c37
SangChan Lee <sangchan82@gmail.com>
2018-01-08 14:17:14 +0800

    Put linked list on

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Sources/LinkedList.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Sources/LinkedList.swift
@@ -1,2 +1,45 @@
 import Foundation
 
+class ListNode {
+    var value : Int?
+    var next : ListNode?
+}
+
+class LinkedList {
+    private var head : ListNode?
+    private var tail: ListNode? {
+        if var node = head {
+            while let next = node.next {
+                node = next
+            }
+            return node
+        } else {
+            return nil
+        }
+    }
+    
+    func addLink(value: Int) {
+        let newNode : ListNode = ListNode()
+        newNode.value = value
+        if let lastNode = tail {
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+    
+    func printAllkeys() {
+        guard let firstNode = head else { return }
+        printNode(firstNode)
+    }
+    
+    fileprivate func printNode(_ node : ListNode) {
+        if let value = node.value {
+            print("value = \(value)")
+        }
+        
+        if let next = node.next {
+            printNode(next)
+        }
+    }
+}
