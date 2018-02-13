c0d3e08c6da6ee7e81fd03fb6139249f60c0c21d
SangChan Lee <sangchan82@gmail.com>
2018-02-13 10:34:37 +0800

    add test case

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -786,3 +786,15 @@ class LRUCache {
 
     }
 }
+
+let cache : LRUCache = LRUCache(capacity: 2)
+
+cache.put(1, value: 1)
+cache.put(2, value: 2)
+cache.get(1) // returns 1
+cache.put(3, value: 3) // evicts key 2
+cache.get(2) // returns -1 (not found)
+cache.put(4, value: 4) // evicts key 1
+cache.get(1) // return -1 (not found)
+cache.get(3) // returns 3
+cache.get(4) // returns 4
