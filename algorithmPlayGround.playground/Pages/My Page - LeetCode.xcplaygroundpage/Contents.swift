84f5939746228d5682119e52fda8a1e6371dd559
SangChan Lee <sangchan82@gmail.com>
2018-05-08 18:55:02 +0900

    another answer

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -1141,3 +1141,19 @@ func invertTree(_ root: TreeNode?) -> TreeNode? {
     newRoot.right = left
     return newRoot;
 }
+
+func invertTreeWithQueue(_ root: TreeNode?) -> TreeNode? {
+    guard let newRoot = root else { return nil }
+//    if (root == null) return null;
+//    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+//    queue.add(root);
+//    while (!queue.isEmpty()) {
+//        TreeNode current = queue.poll();
+//        TreeNode temp = current.left;
+//        current.left = current.right;
+//        current.right = temp;
+//        if (current.left != null) queue.add(current.left);
+//        if (current.right != null) queue.add(current.right);
+//    }
+//    return root;
+}
