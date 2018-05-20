65f014647a8c535aeef19d889d380017ac644029
SangChan Lee <sangchan82@gmail.com>
2018-05-20 23:04:54 +0900

    mock answer

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -1217,3 +1217,7 @@ class QueueUsingStack {
  
  Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
  */
+
+func deleteNode(_ list: LinkedList) -> LinkedList {
+    return LinkedList()
+}
