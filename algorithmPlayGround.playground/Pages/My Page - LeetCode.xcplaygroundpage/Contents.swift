2033e7e693472d8b0ea471432c18a1aa709093c4
SangChan Lee <sangchan82@gmail.com>
2018-05-24 14:46:43 +0900

    add value for this

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -1218,6 +1218,8 @@ class QueueUsingStack {
  Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
  */
 
-func deleteNode(_ list: LinkedList) -> LinkedList {
+func deleteNode(_ node: LinkedList) -> LinkedList {
+    //node.val = node.next.val;
+    //node.next = node.next.next;
     return LinkedList()
 }
