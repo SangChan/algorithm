083941c5c8212b1dac6e6776d1a465e8f0fc44fb
SangChan Lee <sangchan82@gmail.com>
2018-06-04 22:52:55 +0900

    bring from answer

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -1254,6 +1254,23 @@ func isAnagram(_ s: String, _ t: String) -> Bool {
     }
     return true
 }
+/*
+public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    int[] counter = new int[26];
+    for (int i = 0; i < s.length(); i++) {
+        counter[s.charAt(i) - 'a']++;
+        counter[t.charAt(i) - 'a']--;
+    }
+    for (int count : counter) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
+}*/
 
 isAnagram("anagram", "nagaram")
 isAnagram("rat", "car")
