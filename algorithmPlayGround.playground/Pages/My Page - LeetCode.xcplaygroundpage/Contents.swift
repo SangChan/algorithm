be4e8f1011f15b1d50d108a50ca961236261bff1
SangChan Lee <sangchan82@gmail.com>
2018-06-13 10:55:18 +0900

    there's new problem

--- algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - LeetCode.xcplaygroundpage/Contents.swift
@@ -1279,3 +1279,20 @@ func isAnagram(_ s: String, _ t: String) -> Bool {
 
 isAnagram("anagram", "nagaram")
 isAnagram("rat", "car")
+
+/*
+ 268. Missing Number
+ Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ 
+ Example 1:
+    Input: [3,0,1]
+    Output: 2
+ 
+ Example 2:
+    Input: [9,6,4,2,3,5,7,0,1]
+    Output: 8
+ */
+
+func missingNumber(_ nums: [Int]) -> Int {
+    return Int.min
+}
