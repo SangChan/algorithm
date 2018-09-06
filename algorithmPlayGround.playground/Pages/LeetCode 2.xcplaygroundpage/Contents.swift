74127fa4a93d363c50a96587caaf99570cea9848
SangChan Lee <sangchan82@gmail.com>
2018-09-06 11:48:58 +0900

    put answer from solution

--- algorithmPlayGround.playground/Pages/LeetCode 2.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 2.xcplaygroundpage/Contents.swift
@@ -95,4 +95,25 @@ findAndReplacePattern(words, pattern) // return ["mee", "aqq"]
 
 func zigzagConvert(_ s: String, _ numRows: Int) -> String {
     return ""
+    /*
+     sort by row
+     if (numRows == 1) return s;
+     
+     List<StringBuilder> rows = new ArrayList<>();
+     for (int i = 0; i < Math.min(numRows, s.length()); i++)
+     rows.add(new StringBuilder());
+     
+     int curRow = 0;
+     boolean goingDown = false;
+     
+     for (char c : s.toCharArray()) {
+     rows.get(curRow).append(c);
+     if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+     curRow += goingDown ? 1 : -1;
+     }
+     
+     StringBuilder ret = new StringBuilder();
+     for (StringBuilder row : rows) ret.append(row);
+     return ret.toString();
+     */
 }
