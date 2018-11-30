4094801767a87ed9c8b99a55d3d7d9a72134904c
SangChan Lee <sangchan82@gmail.com>
2018-11-30 22:57:45 +0900

    add pseudo code for quick sort

--- algorithmPlayGround.playground/Pages/Sorting.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/Sorting.xcplaygroundpage/Contents.swift
@@ -60,6 +60,48 @@ func quickSort(_ sortList : inout Array<Int>) {
     
 }
 
+/*
+ function partition(a, left, right, pivotIndex)
+ pivotValue := a[pivotIndex]
+ swap(a[pivotIndex], a[right]) // 피벗을 끝으로 옮겨 놓는다.
+ storeIndex := left
+ for i from left to right-1
+ if a[i] <= pivotValue then
+ swap(a[storeIndex], a[i])
+ storeIndex := storeIndex + 1
+ swap(a[right], a[storeIndex]) // 피벗을 두 리스트 사이에 위치시킨다.
+ return storeIndex
+ 
+ void quickSort(int arr[], int left, int right) {
+ int i = left, j = right;
+ int pivot = arr[(left + right) / 2];
+ int temp;
+ do
+ {
+ while (arr[i] < pivot)
+ i++;
+ while (arr[j] > pivot)
+ j--;
+ if (i<= j)
+ {
+ temp = arr[i];
+ arr[i] = arr[j];
+ arr[j] = temp;
+ i++;
+ j--;
+ }
+ } while (i<= j);
+ 
+ /* recursion */
+ if (left < j)
+ quickSort(arr, left, j);
+ 
+ if (i < right)
+ quickSort(arr, i, right);
+ }
+ 
+ */
+
 func mergeSort() {
     
 }
