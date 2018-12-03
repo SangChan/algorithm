b33565e4cb488ad03b90b959416808e8705a08c5
SangChan Lee <sangchan82@gmail.com>
2018-12-03 12:11:58 +0900

    add another lang version

--- algorithmPlayGround.playground/Pages/Sorting.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/Sorting.xcplaygroundpage/Contents.swift
@@ -60,7 +60,7 @@ func quickSort(_ arr : [Int], left : Int, right : Int) {
     var i = left
     var j = right
     var pivot = arr[(left + right) / 2]
-    var temp : Int 
+    var temp : Int
     
 }
 
@@ -102,6 +102,27 @@ func quickSort(_ arr : [Int], left : Int, right : Int) {
  
  if (i < right)
  quickSort(arr, i, right);
+ }
+ 
+ public void quickSort(int[] arr, int left, int right) {
+ int i, j, pivot, tmp;
+ if (left < right) {
+ i = left;   j = right;
+ pivot = arr[(left+right)/2];
+ //분할 과정
+ while (i < j) {
+ while (arr[j] > pivot) j--;
+ // 이 부분에서 arr[j-1]에 접근해서 익셉션 발생가능함.
+ while (i < j && arr[i] <= pivot) i++;
+ 
+ tmp = arr[i];
+ arr[i] = arr[j];
+ arr[j] = tmp;
+ }
+ //정렬 과정
+ quickSort(arr, left, i - 1);
+ quickSort(arr, i + 1, right);
+ }
  }
  
  */
