257ac9469db2b0c94e9132ab3fad937fedf351e8
Sangchan Lee <sangchan.lee@ef.com>
2019-04-22 15:56:58 +0900

    add some

--- algorithmPlayGround.playground/Pages/My Page - Request.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/My Page - Request.xcplaygroundpage/Contents.swift
@@ -27,3 +27,120 @@ protocol RequestBuilderProtocol {
     static func get(_ url: URL) -> URLRequest
     static func get(_ url: URL, token: String, sessionid: String) -> URLRequest
 }
+
+class RequestBuilder : RequestBuilderProtocol {
+    static var model: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8 , value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }()
+    
+    
+    static var idiom: String = {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            return "Phone"
+        case .pad:
+            return "Tablet"
+        case .carPlay:
+            return "Car"
+        case .tv:
+            return "TV"
+        default:
+            return "Unknown"
+        }
+    }()
+    
+    static var identification: String = {
+        return "\(RequestBuilder.model)/iOS\(UIDevice.current.systemVersion)/\(RequestBuilder.idiom)"
+    }()
+    
+    static func post(_ url: URL) -> URLRequest {
+        var r = URLRequest(url: url)
+        r.setValue(RequestBuilder.identification, forHTTPHeaderField: "deviceMode")
+        r.httpMethod = "POST"
+        r.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        r.addValue("application/json", forHTTPHeaderField: "Accept")
+        return r
+    }
+    
+    static func post(_ url: URL, token: String, sessionid: String) -> URLRequest {
+        var r = URLRequest(url: url)
+        r.setValue(token, forHTTPHeaderField: "token")
+        r.setValue(sessionid, forHTTPHeaderField: "sessionid")
+        r.setValue(RequestBuilder.identification, forHTTPHeaderField: "deviceMode")
+        r.httpMethod = "POST"
+        r.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        r.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        return r
+    }
+    
+    static func get(_ url: URL) -> URLRequest {
+        var r = URLRequest(url: url)
+        r.setValue(RequestBuilder.identification, forHTTPHeaderField: "deviceMode")
+        r.httpMethod = "GET"
+        r.addValue("application/json", forHTTPHeaderField: "Accept")
+        return r
+    }
+    
+    static func get(_ url: URL, token: String, sessionid: String) -> URLRequest {
+        var r = URLRequest(url: url)
+        r.setValue(token, forHTTPHeaderField: "token")
+        r.setValue(sessionid, forHTTPHeaderField: "sessionid")
+        r.setValue(RequestBuilder.identification, forHTTPHeaderField: "deviceMode")
+        r.httpMethod = "GET"
+        r.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        return r
+    }
+    
+    
+}
+
+struct LoginRequest : Codable {
+    let userName    : String
+    let password    : String
+}
+
+struct LoginResponse : Codable, LocalResponse {
+    var token: String
+    var sessionId: String
+    var memberId: Int
+    var dataStore: String
+    var errorCode : Int
+    
+    static func documentsURL() -> URL? {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    }
+    
+    static func save(_ loginResponse : LoginResponse) {
+        guard let docsUrl = LoginResponse.documentsURL() else { return }
+        let url = docsUrl.appendingPathComponent("\(LoginResponse.self)")
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(loginResponse)
+            try data.write(to: url, options: [])
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    static func load() -> LoginResponse? {
+        guard let docsUrl = LoginResponse.documentsURL() else { return .none }
+        let url = docsUrl.appendingPathComponent("\(LoginResponse.self)")
+        let decoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: url, options: [])
+            let loginResponse = try decoder.decode(LoginResponse.self, from: data)
+            return loginResponse
+        } catch {
+            return .none
+        }
+    }
+}
