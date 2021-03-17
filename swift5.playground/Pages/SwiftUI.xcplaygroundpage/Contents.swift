c154a4c7e73e67a231f31aa94b5450acfc3aeaeb
Sangchan <sangchan82@gmail.com>
2021-03-17 14:27:17 +0900

    add swiftui test page

---
+++ swift5.playground/Pages/SwiftUI.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+import SwiftUI
+
+struct Episode {
+    let title : String
+    let showTitle : String
+}
+
+struct PlayButton: View {
+    @Binding var isPlaying: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.isPlaying.toggle()
+        }) {
+            Image(systemName: isPlaying ? "pause.circle" : "play.circle")
+        }
+    }
+}
+
+struct PlayerView: View {
+    var episode: Episode
+    @State private var isPlaying: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text(episode.title)
+            Text(episode.showTitle)
+            PlayButton(isPlaying: $isPlaying)
+        }
+    }
+}
