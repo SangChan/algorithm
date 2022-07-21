78209049c8a9acc42d90a1a7bf12041a9c3415bb
Sangchan <sangchan82@gmail.com>
2022-07-21 20:29:03 +0900

    add new

--- algorithmPlayGround.playground/Pages/Remind.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/Remind.xcplaygroundpage/Contents.swift
@@ -67,13 +67,13 @@ stackString.pop()
 stackString.pop()
 stackString.pop()
 
-class StackNode<T> : Equatable where T: Equatable {
-    static func == (lhs: StackNode<T>, rhs: StackNode<T>) -> Bool {
+class LinkedListNode<T> : Equatable where T: Equatable {
+    static func == (lhs: LinkedListNode<T>, rhs: LinkedListNode<T>) -> Bool {
         return lhs.data == rhs.data && lhs.next == rhs.next
     }
     
     let data : T
-    var next : StackNode<T>? = nil
+    var next : LinkedListNode<T>? = nil
     
     init(with value: T) {
         self.data = value
@@ -81,10 +81,10 @@ class StackNode<T> : Equatable where T: Equatable {
 }
 
 class StackByLinkedList<T> : StackProtocol where T : Equatable {
-    private var top : StackNode<T>?
+    private var top : LinkedListNode<T>?
     
     func push(_ item: T) {
-        let t : StackNode<T> = StackNode<T>.init(with: item)
+        let t : LinkedListNode<T> = LinkedListNode<T>.init(with: item)
         t.next = top
         top = t
     }
@@ -105,7 +105,7 @@ class StackByLinkedList<T> : StackProtocol where T : Equatable {
 }
 
 var stackNode = StackByLinkedList<Int>()
-stackNode.push(1) 
+stackNode.push(1)
 stackNode.push(2)
 stackNode.push(3)
 stackNode.push(4)
@@ -122,7 +122,101 @@ stackNode.pop()
 stackNode.pop()
 stackNode.pop()
 
+// Heap
+
+class HeapType {
+    private var heap: [Int] = [Int].init(repeating: Int.max, count: 20)
+    private var size : Int
+    
+    init(with size: Int) {
+        self.size = size
+    }
+    
+    //max heap
+    func insert(_ item: Int) {
+        size += 1
+        var index : Int = size
+        
+        while(index != 0 && item > heap[index/2]) {
+            heap[index] = heap[index/2]
+            index /= 2
+        }
+        heap[index] = item
+    }
+}
+
+var heap1 : HeapType = .init(with: 0)
+heap1.insert(1)
+heap1.insert(2)
+heap1.insert(2)
+heap1.insert(3)
+heap1.insert(3)
+heap1.insert(4)
+heap1.insert(5)
+heap1.insert(6)
+heap1.insert(7)
+heap1.insert(9)
+heap1.insert(8)
+
 // Queue
+
+protocol QueueProtocol {
+    associatedtype T : Equatable
+    func add(_ item: T)
+    func remove() -> T?
+    func peek() -> T?
+    func isEmpty() -> Bool
+}
+
+class QueueByNode<T> : QueueProtocol where T : Equatable {
+    private var first: LinkedListNode<T>? = nil
+    private var last: LinkedListNode<T>? = nil
+
+    func add(_ item: T) {
+        print("add item \(item)")
+        let t : LinkedListNode<T> = .init(with: item)
+        if last != nil { last?.next = t }
+        last = t
+        if first == nil { first = last }
+        print("first = \(first?.data), first.next = \(first?.next?.data), first.next = \(first?.next?.next?.data), first.next = \(first?.next?.next?.next?.data), first.next = \(first?.next?.next?.next?.next?.data)")
+        print("last = \(last?.data), last.next = \(last?.next?.data)")
+    }
+    
+    func remove() -> T? {
+        guard first != nil else { return nil }
+        let data : T = first!.data
+        first = first!.next
+        
+        if first == nil { last = nil }
+        return data
+    }
+    
+    func peek() -> T? {
+        return first?.data
+    }
+    
+    func isEmpty() -> Bool {
+        return first == nil
+    }
+}
+
+let queue : QueueByNode<Int> = .init()
+queue.add(1)
+queue.add(2)
+queue.add(3)
+queue.add(4)
+queue.add(5)
+queue.peek()
+queue.peek()
+queue.peek()
+queue.remove()
+queue.remove()
+queue.remove()
+queue.remove()
+queue.remove()
+queue.remove()
+queue.remove()
+queue.remove()
 
 // Binary Search
 
