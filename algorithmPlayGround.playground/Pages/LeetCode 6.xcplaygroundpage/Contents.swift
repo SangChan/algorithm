d81d92ec15b03a83a1e1a0eef1fb7bdda6d98f1e
Sangchan <sangchan82@gmail.com>
2022-07-24 11:55:47 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -640,5 +640,132 @@ func divide(_ dividend: Int, _ divisor: Int) -> Int {
 
 divide(10, 3)
 
-10 >> 4
-10 << 3
+/*
+ 36. Valid Sudoku
+ Medium
+
+ Share
+ Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+ Each row must contain the digits 1-9 without repetition.
+ Each column must contain the digits 1-9 without repetition.
+ Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ Note:
+
+ A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ Only the filled cells need to be validated according to the mentioned rules.
+ 
+ Input: board =
+ [["5","3",".",".","7",".",".",".","."]
+ ,["6",".",".","1","9","5",".",".","."]
+ ,[".","9","8",".",".",".",".","6","."]
+ ,["8",".",".",".","6",".",".",".","3"]
+ ,["4",".",".","8",".","3",".",".","1"]
+ ,["7",".",".",".","2",".",".",".","6"]
+ ,[".","6",".",".",".",".","2","8","."]
+ ,[".",".",".","4","1","9",".",".","5"]
+ ,[".",".",".",".","8",".",".","7","9"]]
+ Output: true
+ 
+ Input: board =
+ [["8","3",".",".","7",".",".",".","."]
+ ,["6",".",".","1","9","5",".",".","."]
+ ,[".","9","8",".",".",".",".","6","."]
+ ,["8",".",".",".","6",".",".",".","3"]
+ ,["4",".",".","8",".","3",".",".","1"]
+ ,["7",".",".",".","2",".",".",".","6"]
+ ,[".","6",".",".",".",".","2","8","."]
+ ,[".",".",".","4","1","9",".",".","5"]
+ ,[".",".",".",".","8",".",".","7","9"]]
+ Output: false
+ Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ */
+
+func isValidSudoku(_ board: [[Character]]) -> Bool {
+    var nums : [Character] = []
+    // 1x9 체크
+    for i in 0 ..< board.count {
+        nums = []
+        let line : [Character] = board[i]
+        for j in 0 ..< line.count {
+            let char : Character = line[j]
+            if char != "." {
+                if nums.firstIndex(of: char) != nil {
+                    return false
+                }
+                nums.append(char)
+            }
+        }
+    }
+    
+    // 9x1 체크
+    
+    var newBoard : [[Character]] = .init(repeating: .init(repeating: .init("."), count: board.count), count: board.count)
+    for i in 0 ..< board.count {
+        let line : [Character] = board[i]
+        for j in 0 ..< line.count {
+            newBoard[j][i] = board[i][j]
+        }
+    }
+    
+    for i in 0 ..< newBoard.count {
+        nums = []
+        let line : [Character] = newBoard[i]
+        for j in 0 ..< line.count {
+            let char : Character = line[j]
+            if char != "." {
+                if nums.firstIndex(of: char) != nil {
+                    return false
+                }
+                nums.append(char)
+            }
+        }
+    }
+    
+    // 3x3 체크
+    
+    for i in 0 ..< board.count / 3 {
+        for j in 0 ..< board.count / 3 {
+            nums = []
+            for k in 0 ..< 3 {
+                for l in 0 ..< 3 {
+                    let char = board[(i*3)+k][(j*3)+l]
+                    if char != "." {
+                        if nums.firstIndex(of: char) != nil {
+                            return false
+                        }
+                        nums.append(char)
+                    }
+                }
+            }
+        }
+    }
+    
+    return true
+}
+
+let rightBoard : [[Character]] = [
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+isValidSudoku(rightBoard)
+
+let wrongBoard : [[Character]] = [
+    ["8","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+//isValidSudoku(wrongBoard)
