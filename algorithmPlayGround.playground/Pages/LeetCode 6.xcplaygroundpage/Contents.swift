3be6a187dfc6aafcd46f78c8a744c7cc03842b38
Sangchan <sangchan82@gmail.com>
2022-07-25 22:39:51 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -768,4 +768,113 @@ let wrongBoard : [[Character]] = [
     [".",".",".","4","1","9",".",".","5"],
     [".",".",".",".","8",".",".","7","9"]
 ]
-//isValidSudoku(wrongBoard)
+isValidSudoku(wrongBoard)
+
+/*
+ 240. Search a 2D Matrix II
+ Medium
+
+ Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+ Integers in each row are sorted in ascending from left to right.
+ Integers in each column are sorted in ascending from top to bottom.
+  
+
+ Example 1:
+
+
+ Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ Output: true
+ Example 2:
+
+
+ Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ Output: false
+  
+
+ Constraints:
+
+ m == matrix.length
+ n == matrix[i].length
+ 1 <= n, m <= 300
+ -109 <= matrix[i][j] <= 109
+ All the integers in each row are sorted in ascending order.
+ All the integers in each column are sorted in ascending order.
+ -109 <= target <= 109
+ 
+ */
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    for nums in matrix {
+        if nums.firstIndex(of: target) != nil {
+            return true
+        }
+    }
+    
+    return false
+}
+
+
+let matrix = [
+    [1,4,7,11,15],
+    [2,5,8,12,19],
+    [3,6,9,16,22],
+    [10,13,14,17,24],
+    [18,21,23,26,30]
+]
+searchMatrix(matrix, 5)
+searchMatrix(matrix, 20)
+
+/*
+ 46. Permutations
+ Medium
+
+ Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3]
+ Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ Example 2:
+
+ Input: nums = [0,1]
+ Output: [[0,1],[1,0]]
+ Example 3:
+
+ Input: nums = [1]
+ Output: [[1]]
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 6
+ -10 <= nums[i] <= 10
+ All the integers of nums are unique.
+ */
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    guard nums.count != 0 else { return [] }
+    
+    // [1]
+    var answer : [[Int]] = []
+    let firstList : [Int] = [nums[0]]
+    answer.append(firstList)
+    
+    // [1] -> [1,2], [2,1]
+    // [1,2], [2,1] -> [3,1,2], [1,3,2], [1,2,3], [3,2,1], [2,3,1], [2,1,3]
+    for i in 1 ..< nums.count {
+        var newAnswer : [[Int]] = []
+        for j in 0 ... i {
+            for list in answer {
+                var newList = list
+                newList.insert(nums[i], at: j)
+                newAnswer.append(newList)
+            }
+        }
+        answer = newAnswer
+    }
+    
+    return answer
+}
+permute([1,2,3])
