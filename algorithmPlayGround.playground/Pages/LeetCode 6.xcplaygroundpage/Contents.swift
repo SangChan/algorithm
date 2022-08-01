1c399b4e1ac020cc8e461260790c186ecdc62c3b
Sangchan <sangchan82@gmail.com>
2022-08-01 19:59:13 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -1500,3 +1500,88 @@ undergroundSystem2.getAverageTime("Leyton", "Paradise"); // return 5.50000, (5 +
 undergroundSystem2.checkIn(2, "Leyton", 21);
 undergroundSystem2.checkOut(2, "Paradise", 30); // Customer 2 "Leyton" -> "Paradise" in 30-21 = 9
 undergroundSystem2.getAverageTime("Leyton", "Paradise"); // return 6.66667, (5 + 6 + 9) / 3 = 6.66667
+
+/*
+ 1472. Design Browser History
+ Medium
+
+ You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+
+ Implement the BrowserHistory class:
+
+ BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+ void visit(string url) Visits url from the current page. It clears up all the forward history.
+ string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+ string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+  
+
+ Example:
+
+ Input:
+ ["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
+ [["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
+ Output:
+ [null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
+
+ Explanation:
+ BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+ browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+ browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+ browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+ browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+ browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+ browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+ browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+ browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+ browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+ browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+  
+
+ Constraints:
+
+ 1 <= homepage.length <= 20
+ 1 <= url.length <= 20
+ 1 <= steps <= 100
+ homepage and url consist of  '.' or lower case English letters.
+ At most 5000 calls will be made to visit, back, and forward.
+ */
+
+class BrowserHistory {
+    private var _history: [String] = []
+    private var _index : Int = 0
+
+    init(_ homepage: String) {
+        _history.append(homepage)
+        _index = 0
+    }
+    
+    func visit(_ url: String) {
+        _index += 1
+        if _history.count >= _index + 1 {
+            _history.removeLast(_history.count - _index)
+        }
+        _history.append(url)
+    }
+    
+    func back(_ steps: Int) -> String {
+        _index = max(0, _index - steps)
+        return _history[_index]
+    }
+    
+    func forward(_ steps: Int) -> String {
+        _index = min(_index + steps, _history.count - 1)
+        return _history[_index]
+    }
+}
+
+var browserHistory = BrowserHistory("leetcode.com");
+browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
