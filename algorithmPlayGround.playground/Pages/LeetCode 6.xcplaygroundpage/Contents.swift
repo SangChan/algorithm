2f1aee84527119dea226ee72af79a056a99d27fa
Sangchan <sangchan82@gmail.com>
2022-08-03 22:40:09 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -1671,3 +1671,76 @@ func dfs(_ m: Int, _ n: Int, _ x: Int, _ y: Int, _ visited: inout [[Bool]], _ in
 exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED")
 exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE")
 exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB")
+
+/*
+ 729. My Calendar I
+ Medium
+ 
+ You are implementing a program to use as your calendar. We can add a new event if adding the event will not cause a double booking.
+
+ A double booking happens when two events have some non-empty intersection (i.e., some moment is common to both events.).
+
+ The event can be represented as a pair of integers start and end that represents a booking on the half-open interval [start, end), the range of real numbers x such that start <= x < end.
+
+ Implement the MyCalendar class:
+
+ MyCalendar() Initializes the calendar object.
+ boolean book(int start, int end) Returns true if the event can be added to the calendar successfully without causing a double booking. Otherwise, return false and do not add the event to the calendar.
+  
+
+ Example 1:
+
+ Input
+ ["MyCalendar", "book", "book", "book"]
+ [[], [10, 20], [15, 25], [20, 30]]
+ Output
+ [null, true, false, true]
+
+ Explanation
+ MyCalendar myCalendar = new MyCalendar();
+ myCalendar.book(10, 20); // return True
+ myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+ myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+  
+
+ Constraints:
+
+ 0 <= start < end <= 109
+ At most 1000 calls will be made to book.
+ */
+
+class MyCalendar {
+    struct Event {
+        let start : Int
+        let end : Int
+        
+        func check(with event: Event) -> Bool {
+            if event.end <= self.start || event.start >= self.end { return false }
+            return true
+        }
+    }
+    
+    private var _events : [Event]
+    
+    init() {
+        _events = []
+    }
+    
+    func book(_ start: Int, _ end: Int) -> Bool {
+        let newEvent : Event = .init(start: start, end: end)
+        
+        for event in _events {
+            if event.check(with: newEvent) == true {
+                return false
+            }
+        }
+        _events.append(newEvent)
+        
+        return true
+    }
+}
+
+let myCalendar : MyCalendar = .init()
+myCalendar.book(10, 20); // return True
+myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
+myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
