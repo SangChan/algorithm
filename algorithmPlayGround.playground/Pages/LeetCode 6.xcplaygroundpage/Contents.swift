a9cd98996ffa8815db33e0d011392a0688f4d410
Sangchan <sangchan82@gmail.com>
2022-08-04 21:10:12 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -1744,3 +1744,60 @@ let myCalendar : MyCalendar = .init()
 myCalendar.book(10, 20); // return True
 myCalendar.book(15, 25); // return False, It can not be booked because time 15 is already booked by another event.
 myCalendar.book(20, 30); // return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+/*
+ 412. Fizz Buzz
+ Easy
+
+ 718
+
+ 118
+
+ Add to List
+
+ Share
+ Given an integer n, return a string array answer (1-indexed) where:
+
+ answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ answer[i] == "Fizz" if i is divisible by 3.
+ answer[i] == "Buzz" if i is divisible by 5.
+ answer[i] == i (as a string) if none of the above conditions are true.
+  
+
+ Example 1:
+
+ Input: n = 3
+ Output: ["1","2","Fizz"]
+ Example 2:
+
+ Input: n = 5
+ Output: ["1","2","Fizz","4","Buzz"]
+ Example 3:
+
+ Input: n = 15
+ Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+  
+
+ Constraints:
+
+ 1 <= n <= 104
+ Accepted
+ 694,722
+ Submissions
+
+ */
+
+func fizzBuzz(_ n: Int) -> [String] {
+    var result : [String] = []
+    for i in 1 ... n {
+        if i % 15 == 0 { result.append("FizzBuzz") }
+        else if i % 5 == 0 { result.append("Buzz") }
+        else if i % 3 == 0 { result.append("Fizz") }
+        else { result.append("\(i)") }
+    }
+    return result
+}
+
+fizzBuzz(3)
+fizzBuzz(5)
+fizzBuzz(15)
