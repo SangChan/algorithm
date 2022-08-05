135f2c7c0a341a58b8556e6d011cc2f67e63455f
Sangchan <sangchan82@gmail.com>
2022-08-05 17:33:19 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -1801,3 +1801,66 @@ func fizzBuzz(_ n: Int) -> [String] {
 fizzBuzz(3)
 fizzBuzz(5)
 fizzBuzz(15)
+
+/*
+ 94. Binary Tree Inorder Traversal
+ 
+ Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+  
+
+ Example 1:
+
+
+ Input: root = [1,null,2,3]
+ Output: [1,3,2]
+ Example 2:
+
+ Input: root = []
+ Output: []
+ Example 3:
+
+ Input: root = [1]
+ Output: [1]
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [0, 100].
+ -100 <= Node.val <= 100
+  
+
+ Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    guard let root = root else {return []}
+       var result = [Int]()
+       helper(root, &result)
+       return result
+   }
+
+func helper(_ root :TreeNode?, _ result :inout [Int]) {
+   guard let root = root else {return}
+   
+   helper(root.left, &result)
+   result.append(root.val)
+   helper(root.right, &result)
+}
+
