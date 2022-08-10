4e41d23c4481b2d7e909737618802dce831626db
Sangchan <sangchan82@gmail.com>
2022-08-10 15:35:30 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2236,3 +2236,46 @@ func numFactoredBinaryTrees(_ arr: [Int]) -> Int {
 }
 
 numFactoredBinaryTrees([2,4])
+
+/*
+ 108. Convert Sorted Array to Binary Search Tree
+ 
+ Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+ A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+  
+
+ Example 1:
+
+
+ Input: nums = [-10,-3,0,5,9]
+ Output: [0,-3,9,-10,null,5]
+ Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+ Example 2:
+
+
+ Input: nums = [1,3]
+ Output: [3,1]
+ Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 104
+ -104 <= nums[i] <= 104
+ nums is sorted in a strictly increasing order.
+ */
+
+func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+    func convert(_ left : Int, _ right : Int) -> TreeNode? {
+        guard left <= right else { return nil }
+        let mid : Int = (left + right) / 2
+        var node : TreeNode = TreeNode(nums[mid])
+        node.left = convert(left, mid - 1)
+        node.right = convert(mid + 1, right)
+        return node
+    }
+    return convert(0, nums.count - 1)
+}
