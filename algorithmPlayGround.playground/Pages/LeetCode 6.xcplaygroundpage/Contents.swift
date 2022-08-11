6f3e32a53b01b74ae99b0ceb4bede73e67f4d2ed
Sangchan <sangchan82@gmail.com>
2022-08-11 17:48:35 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2320,3 +2320,45 @@ findIsland([
     [0,0,0,0,1],
     [0,0,0,1,1]
 ])
+
+/*
+ 98. Validate Binary Search Tree
+ Medium
+
+ Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+ A valid BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than the node's key.
+ The right subtree of a node contains only nodes with keys greater than the node's key.
+ Both the left and right subtrees must also be binary search trees.
+  
+
+ Example 1:
+
+
+ Input: root = [2,1,3]
+ Output: true
+ Example 2:
+
+
+ Input: root = [5,1,4,null,null,3,6]
+ Output: false
+ Explanation: The root node's value is 5 but its right child's value is 4.
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [1, 104].
+ -231 <= Node.val <= 231 - 1
+ */
+
+func isValidBST(_ root: TreeNode?) -> Bool {
+    func validate(_ node: TreeNode?, _ low : Int = Int.min, _ high : Int = Int.max) -> Bool {
+        guard let node = node else { return true }
+        if node.val <= low || node.val >= high { return false }
+        return validate(node.right, node.val, high) && validate(node.left, low, node.val)
+    }
+    
+    return validate(root)
+}
