d5a37f55b9821742348c6c1c07ac159bf7d8ca82
Sangchan <sangchan82@gmail.com>
2022-08-13 14:55:34 +0900

    postpone

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2450,7 +2450,11 @@ func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> 
  */
 
 func findSubstring(_ s: String, _ words: [String]) -> [Int] {
-    func slidingWindow(_ i : Int, _ s : String, _ answer : inout [Int]) {
+    func slidingWindow(_ left : Int, _ s : String, _ answer : inout [Int]) {
+        var wordsFound : [String: Int] = [:]
+        var wordsUsed : Int = 0
+        var excessWord : Bool = false
+        
         
     }
     
@@ -2477,53 +2481,3 @@ func findSubstring(_ s: String, _ words: [String]) -> [Int] {
 }
 
 findSubstring("barfoothefoobarman", ["foo","bar"]) // [0,9]
-
-
-private void slidingWindow(int left, String s, List<Integer> answer) {
-       HashMap<String, Integer> wordsFound = new HashMap<>();
-       int wordsUsed = 0;
-       boolean excessWord = false;
-       
-       // Do the same iteration pattern as the previous approach - iterate
-       // word_length at a time, and at each iteration we focus on one word
-       for (int right = left; right <= n - wordLength; right += wordLength) {
-           
-           String sub = s.substring(right, right + wordLength);
-           if (!wordCount.containsKey(sub)) {
-               // Mismatched word - reset the window
-               wordsFound.clear();
-               wordsUsed = 0;
-               excessWord = false;
-               left = right + wordLength;
-           } else {
-               // If we reached max window size or have an excess word
-               while (right - left == substringSize || excessWord) {
-                   String leftmostWord = s.substring(left, left + wordLength);
-                   left += wordLength;
-                   wordsFound.put(leftmostWord, wordsFound.get(leftmostWord) - 1);
-
-                   if (wordsFound.get(leftmostWord) >= wordCount.get(leftmostWord)) {
-                       // This word was an excess word
-                       excessWord = false;
-                   } else {
-                       // Otherwise we actually needed it
-                       wordsUsed--;
-                   }
-               }
-               
-               // Keep track of how many times this word occurs in the window
-               wordsFound.put(sub, wordsFound.getOrDefault(sub, 0) + 1);
-               if (wordsFound.get(sub) <= wordCount.get(sub)) {
-                   wordsUsed++;
-               } else {
-                   // Found too many instances already
-                   excessWord = true;
-               }
-               
-               if (wordsUsed == k && !excessWord) {
-                   // Found a valid substring
-                   answer.add(left);
-               }
-           }
-       }
-   }
