657ce02dc76627a3531f13aca42e3c622019685e
Sangchan <sangchan82@gmail.com>
2022-08-14 19:44:08 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2481,3 +2481,80 @@ func findSubstring(_ s: String, _ words: [String]) -> [Int] {
 }
 
 findSubstring("barfoothefoobarman", ["foo","bar"]) // [0,9]
+
+/*
+ 126. Word Ladder II
+ Hard
+ 
+ A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+ Every adjacent pair of words differs by a single letter.
+ Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+ sk == endWord
+ Given two words, beginWord and endWord, and a dictionary wordList, return all the shortest transformation sequences from beginWord to endWord, or an empty list if no such sequence exists. Each sequence should be returned as a list of the words [beginWord, s1, s2, ..., sk].
+
+  
+
+ Example 1:
+
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ Output: [["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+ Explanation: There are 2 shortest transformation sequences:
+ "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+ "hit" -> "hot" -> "lot" -> "log" -> "cog"
+ Example 2:
+
+ Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ Output: []
+ Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+  
+
+ Constraints:
+
+ 1 <= beginWord.length <= 5
+ endWord.length == beginWord.length
+ 1 <= wordList.length <= 500
+ wordList[i].length == beginWord.length
+ beginWord, endWord, and wordList[i] consist of lowercase English letters.
+ beginWord != endWord
+ All the words in wordList are unique.
+ */
+
+func findLadders(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> [[String]] {
+    let wordSet = Set(wordList)
+    if wordSet.contains(endWord) == false {
+        return []
+    }
+    var res = [[String]]()
+    var queue: [[String]] = [[beginWord]]
+    var found = false
+    var tempSeen = Set<String>()
+    var visited = Set<String>()
+    
+    while queue.isEmpty == false && found == false {
+        let temp = queue
+        queue.removeAll()
+        visited.formUnion(tempSeen)
+        tempSeen.removeAll()
+        
+        for path in temp {
+            let lastWord = Array(path.last!)
+            if path.last! == endWord {
+                found = true
+                res.append(path)
+            }
+
+            for i in 0..<lastWord.endIndex {
+                for char in "abcdefghijklmnopqrstuvwxyz" {
+                    let newWord = String(lastWord[0..<i] + [char] + lastWord[i+1..<lastWord.endIndex])
+                    if wordSet.contains(newWord) && visited.contains(newWord) == false {
+                        tempSeen.insert(newWord)
+                        queue.append(path+[newWord])
+                    }
+                }
+            }
+        }
+    }
+    return res
+}
+findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"]) //[["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
