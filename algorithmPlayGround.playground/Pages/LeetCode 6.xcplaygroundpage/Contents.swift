9c6bd0fd8de2053ed33185db891ad2e5c631d054
Sangchan <sangchan82@gmail.com>
2022-08-16 09:47:45 +0900

    add new : time limit exceeded

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2618,3 +2618,42 @@ func lengthOfLongestSubstring(_ s: String) -> Int {
 lengthOfLongestSubstring("abcabcbb") // 3. abc
 lengthOfLongestSubstring("bbbbb") // 1. b
 lengthOfLongestSubstring("pwwkew") // 3. wke
+
+/*
+ 387. First Unique Character in a String
+ Easy
+
+ Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+  
+
+ Example 1:
+
+ Input: s = "leetcode"
+ Output: 0
+ Example 2:
+
+ Input: s = "loveleetcode"
+ Output: 2
+ Example 3:
+
+ Input: s = "aabb"
+ Output: -1
+  
+
+ Constraints:
+
+ 1 <= s.length <= 105
+ s consists of only lowercase English letters.
+ */
+
+func firstUniqChar(_ s: String) -> Int {
+    for (index, char) in s.enumerated() {
+        if s.filter({ $0 == char }).count == 1 { return index }
+    }
+    return -1
+}
+
+firstUniqChar("leetcode") // 0
+firstUniqChar("loveleetcode") // 2
+firstUniqChar("aabb") // -1
