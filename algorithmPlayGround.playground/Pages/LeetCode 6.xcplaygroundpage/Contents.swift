251f12bf45d4677d4182ea9821ceda504ae9d65a
Sangchan <sangchan82@gmail.com>
2022-08-17 10:03:41 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2668,3 +2668,94 @@ func firstUniqChar(_ s: String) -> Int {
 firstUniqChar("leetcode") // 0
 firstUniqChar("loveleetcode") // 2
 firstUniqChar("aabb") // -1
+/*
+ 804. Unique Morse Code Words
+ Easy
+
+ 1341
+
+ 1106
+
+ Add to List
+
+ Share
+ International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+ 'a' maps to ".-",
+ 'b' maps to "-...",
+ 'c' maps to "-.-.", and so on.
+ For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+ [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+ For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+ Return the number of different transformations among all words we have.
+
+  
+
+ Example 1:
+
+ Input: words = ["gin","zen","gig","msg"]
+ Output: 2
+ Explanation: The transformation of each word is:
+ "gin" -> "--...-."
+ "zen" -> "--...-."
+ "gig" -> "--...--."
+ "msg" -> "--...--."
+ There are 2 different transformations: "--...-." and "--...--.".
+ Example 2:
+
+ Input: words = ["a"]
+ Output: 1
+  
+
+ Constraints:
+
+ 1 <= words.length <= 100
+ 1 <= words[i].length <= 12
+ words[i] consists of lowercase English letters.
+ */
+
+func uniqueMorseRepresentations(_ words: [String]) -> Int {
+    let morseTable : [Character : String] = [
+        "a" : ".-",
+        "b" : "-...",
+        "c" : "-.-.",
+        "d" : "-..",
+        "e" : ".",
+        "f" : "..-.",
+        "g" : "--.",
+        "h" : "....",
+        "i" : "..",
+        "j" : ".---",
+        "k" : "-.-",
+        "l" : ".-..",
+        "m" : "--",
+        "n" : "-.",
+        "o" : "---",
+        "p" : ".--.",
+        "q" : "--.-",
+        "r" : ".-.",
+        "s" : "...",
+        "t" : "-",
+        "u" : "..-",
+        "v" : "...-",
+        "w" : ".--",
+        "x" : "-..-",
+        "y" : "-.--",
+        "z" : "--.."
+    ]
+    
+    var answer : Set<String> = Set<String>()
+    
+    for word in words {
+        var morsed : String = ""
+        for char in word {
+            morsed.append(morseTable[char]!)
+        }
+        answer.insert(morsed)
+    }
+    
+    return answer.count
+}
