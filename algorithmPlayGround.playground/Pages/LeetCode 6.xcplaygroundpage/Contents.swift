227073062dc3d56d4e12d62697c976eb3414d8e5
Sangchan <sangchan82@gmail.com>
2022-08-19 14:11:34 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2813,3 +2813,84 @@ func minSetSize(_ arr: [Int]) -> Int {
 
 minSetSize([3,3,3,3,5,5,5,2,2,7]) // 2
 minSetSize([7,7,7,7,7,7]) // 1
+
+/*
+ 659. Split Array into Consecutive Subsequences
+ Medium
+
+ You are given an integer array nums that is sorted in non-decreasing order.
+
+ Determine if it is possible to split nums into one or more subsequences such that both of the following conditions are true:
+
+ Each subsequence is a consecutive increasing sequence (i.e. each integer is exactly one more than the previous integer).
+ All subsequences have a length of 3 or more.
+ Return true if you can split nums according to the above conditions, or false otherwise.
+
+ A subsequence of an array is a new array that is formed from the original array by deleting some (can be none) of the elements without disturbing the relative positions of the remaining elements. (i.e., [1,3,5] is a subsequence of [1,2,3,4,5] while [1,3,2] is not).
+
+  
+
+ Example 1:
+
+ Input: nums = [1,2,3,3,4,5]
+ Output: true
+ Explanation: nums can be split into the following subsequences:
+ [1,2,3,3,4,5] --> 1, 2, 3
+ [1,2,3,3,4,5] --> 3, 4, 5
+ Example 2:
+
+ Input: nums = [1,2,3,3,4,4,5,5]
+ Output: true
+ Explanation: nums can be split into the following subsequences:
+ [1,2,3,3,4,4,5,5] --> 1, 2, 3, 4, 5
+ [1,2,3,3,4,4,5,5] --> 3, 4, 5
+ Example 3:
+
+ Input: nums = [1,2,3,4,4,5]
+ Output: false
+ Explanation: It is impossible to split nums into consecutive increasing subsequences of length 3 or more.
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 104
+ -1000 <= nums[i] <= 1000
+ nums is sorted in non-decreasing order.
+ */
+
+func isPossible(_ nums: [Int]) -> Bool {
+    guard nums.count >= 3 else { return false }
+            
+    var freqMap = [Int: Int]() // [num: freq]
+    for num in nums { freqMap[num, default: 0] += 1 }
+    
+    var subseqMap = [Int: Int]() // [a_subseq_last_num: freq]
+    
+    for (_, num) in nums.enumerated() {
+        if freqMap[num]! == 0 { continue }
+        
+        // update existing subsequence
+        if subseqMap[num - 1, default: 0] > 0 {
+            subseqMap[num - 1]! -= 1
+            subseqMap[num, default: 0] += 1
+        }
+        // can create a new subsequence
+        else if freqMap[num + 1, default: 0] > 0
+             && freqMap[num + 2, default: 0] > 0 {
+            subseqMap[num + 2, default: 0] += 1
+            
+            freqMap[num + 1]! -= 1
+            freqMap[num + 2]! -= 1
+        } else {
+            return false
+        }
+        
+        freqMap[num]! -= 1
+    }
+    
+    return true
+}
+
+isPossible([1,2,3,3,4,5])
+isPossible([1,2,3,3,4,4,5,5])
+isPossible([1,2,3,4,4,5])
