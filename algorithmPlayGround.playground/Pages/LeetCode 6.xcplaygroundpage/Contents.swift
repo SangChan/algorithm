eb4410c5e8200f223567d30fd9121659e8bc1fbc
Sangchan <sangchan82@gmail.com>
2022-08-22 16:19:28 +0900

    add new'

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2954,3 +2954,43 @@ func longestPalindrome(_ s: String) -> String {
 
 longestPalindrome("babad")
 longestPalindrome("cbbd")
+
+/*
+ 342. Power of Four
+ Easy
+
+ Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+ An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+  
+
+ Example 1:
+
+ Input: n = 16
+ Output: true
+ Example 2:
+
+ Input: n = 5
+ Output: false
+ Example 3:
+
+ Input: n = 1
+ Output: true
+  
+
+ Constraints:
+
+ -231 <= n <= 231 - 1
+  
+
+ Follow up: Could you solve it without loops/recursion?
+ */
+
+func isPowerOfFour(_ n: Int) -> Bool {
+    return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0
+}
+
+isPowerOfFour(16)
+isPowerOfFour(5)
+isPowerOfFour(1)
