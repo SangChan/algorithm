18cac4fcafdf353cf4a91659f9605a314e2b30d0
Sangchan <sangchan82@gmail.com>
2022-08-23 17:20:52 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -2994,3 +2994,70 @@ func isPowerOfFour(_ n: Int) -> Bool {
 isPowerOfFour(16)
 isPowerOfFour(5)
 isPowerOfFour(1)
+
+/*
+ 234. Palindrome Linked List
+ Easy
+
+ Given the head of a singly linked list, return true if it is a palindrome.
+
+  
+
+ Example 1:
+
+
+ Input: head = [1,2,2,1]
+ Output: true
+ Example 2:
+
+
+ Input: head = [1,2]
+ Output: false
+  
+
+ Constraints:
+
+ The number of nodes in the list is in the range [1, 105].
+ 0 <= Node.val <= 9
+  
+
+ Follow up: Could you do it in O(n) time and O(1) space?
+ */
+
+func isPalindrome(_ head: ListNode?) -> Bool {
+    var fast: ListNode? = head
+    var slow: ListNode? = head
+    
+    while fast != nil && fast?.next != nil {
+        fast = fast?.next?.next
+        slow = slow?.next
+    }
+    if fast != nil {
+        slow = slow?.next
+    }
+    slow = reverse(slow)
+    fast = head
+    while slow != nil {
+        if fast?.val != slow?.val {
+            return false
+        }
+        fast = fast?.next
+        slow = slow?.next
+    }
+    
+    return true
+}
+
+func reverse(_ head: ListNode?) -> ListNode? {
+    var head = head
+    var pre: ListNode? = nil
+
+    while head != nil {
+        let next = head?.next
+        head?.next = pre
+        pre = head
+        head = next
+    }
+    
+    return pre
+}
