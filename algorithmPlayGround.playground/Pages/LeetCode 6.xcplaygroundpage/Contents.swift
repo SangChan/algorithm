3c15ebb008efad80213a2bb4b4c71100e61ee5ab
Sangchan <sangchan82@gmail.com>
2022-08-27 12:03:25 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -3245,3 +3245,155 @@ func count(_ n: Int) -> [Int] {
 
 reorderedPowerOf2(1)
 reorderedPowerOf2(24)
+
+/*
+ 363. Max Sum of Rectangle No Larger Than K
+ Hard
+
+ 
+ Given an m x n matrix matrix and an integer k, return the max sum of a rectangle in the matrix such that its sum is no larger than k.
+
+ It is guaranteed that there will be a rectangle with a sum no larger than k.
+
+  
+
+ Example 1:
+
+
+ Input: matrix = [[1,0,1],[0,-2,3]], k = 2
+ Output: 2
+ Explanation: Because the sum of the blue rectangle [[0, 1], [-2, 3]] is 2, and 2 is the max number no larger than k (k = 2).
+ Example 2:
+
+ Input: matrix = [[2,2,-1]], k = 3
+ Output: 3
+  
+
+ Constraints:
+
+ m == matrix.length
+ n == matrix[i].length
+ 1 <= m, n <= 100
+ -100 <= matrix[i][j] <= 100
+ -105 <= k <= 105
+  
+
+ Follow up: What if the number of rows is much larger than the number of columns?
+ */
+
+func maxSumSubmatrix(_ matrix: [[Int]], _ k: Int) -> Int {
+    var res = Int.min
+    let m = matrix.count
+    let n = matrix[0].count
+    
+    
+    for left in 0..<n {
+        var rowsSum = [Int](repeating: 0, count: m)
+        for right in left..<n {
+            
+            for row in 0..<m {
+                rowsSum[row] += matrix[row][right]
+            }
+            res = max(countSubarraysSmallerThenK(rowsSum, k), res)
+        }
+    }
+    
+    return res
+}
+
+func countSubarraysSmallerThenK(_ arr: [Int], _ k: Int) -> Int {
+    var res = Int.min
+    
+    for i in 0..<arr.count {
+        var sum = 0
+        for j in i..<arr.count {
+            sum += arr[j]
+            if sum > res && sum <= k {
+                res = sum
+            }
+        }
+    }
+    
+    return res
+}
+
+/*
+ 6. Zigzag Conversion
+ Medium
+
+ The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+ P   A   H   N
+ A P L S I I G
+ Y   I   R
+ And then read line by line: "PAHNAPLSIIGYIR"
+
+ Write the code that will take a string and make this conversion given a number of rows:
+
+ string convert(string s, int numRows);
+  
+
+ Example 1:
+
+ Input: s = "PAYPALISHIRING", numRows = 3
+ Output: "PAHNAPLSIIGYIR"
+ Example 2:
+
+ Input: s = "PAYPALISHIRING", numRows = 4
+ Output: "PINALSIGYAHRPI"
+ Explanation:
+ P     I    N
+ A   L S  I G
+ Y A   H R
+ P     I
+ Example 3:
+
+ Input: s = "A", numRows = 1
+ Output: "A"
+  
+
+ Constraints:
+
+ 1 <= s.length <= 1000
+ s consists of English letters (lower-case and upper-case), ',' and '.'.
+ 1 <= numRows <= 1000
+ */
+
+func convert(_ s: String, _ numRows: Int) -> String {
+    guard numRows != 1 else { return s }
+       
+    var zigzagArr : [[Character]] = Array(repeating: Array(repeating: "$", count: 0 ), count: numRows)
+    var isTop = true;
+    var isBottom = false;
+    var i = 0;
+  
+
+    for item in s {
+        if isTop {
+            zigzagArr[i].append(item);
+            i += 1 ;
+            if i == numRows {
+                i = (numRows - 2);
+                isTop = false;
+                isBottom =  true;
+            }
+        } else if isBottom {
+            zigzagArr[i].append(item)
+            i -= 1;
+            if i == -1 {
+                i = 1;
+                isTop = true;
+                isBottom = false;
+            }
+        }
+    }
+    var str : String = ""
+    for item2 in zigzagArr {
+
+        str = str + String(item2)
+    }
+    return str;
+}
+
+convert("PAYPALISHIRING", 3)  // PAHNAPLSIIGYIR
+convert("PAYPALISHIRING", 4)  // PINALSIGYAHRPI
