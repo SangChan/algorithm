75ad565f30d61e34b30eb7ef345d949696e7581d
Sangchan <sangchan82@gmail.com>
2022-09-03 06:59:04 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -3680,3 +3680,34 @@ private func dfs(_ root: TreeNode?, _ parentsMax: Int)->Int{
     let currentMax = max(root.val, parentsMax)
     return (root.val >= parentsMax ? 1 : 0) + dfs(root.left, currentMax) + dfs(root.right, currentMax)
 }
+
+/*
+ 637. Average of Levels in Binary Tree
+ Easy
+
+ Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
+  
+
+ Example 1:
+
+
+ Input: root = [3,9,20,null,null,15,7]
+ Output: [3.00000,14.50000,11.00000]
+ Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+ Hence return [3, 14.5, 11].
+ Example 2:
+
+
+ Input: root = [3,9,20,15,7]
+ Output: [3.00000,14.50000,11.00000]
+  
+
+ Constraints:
+
+ The number of nodes in the tree is in the range [1, 104].
+ -231 <= Node.val <= 231 - 1
+ */
+
+func averageOfLevels(_ root: TreeNode?) -> [Double] {
+    return []
+}
