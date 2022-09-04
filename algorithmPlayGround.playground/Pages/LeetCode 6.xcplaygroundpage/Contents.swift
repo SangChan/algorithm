3a35a92a86c0ad3337c2f7986f8220fcfcb14b0e
Sangchan <sangchan82@gmail.com>
2022-09-04 12:12:47 +0900

    add new

--- algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/LeetCode 6.xcplaygroundpage/Contents.swift
@@ -3711,3 +3711,53 @@ private func dfs(_ root: TreeNode?, _ parentsMax: Int)->Int{
 func averageOfLevels(_ root: TreeNode?) -> [Double] {
     return []
 }
+ 
+/*
+ DFS
+ public List < Double > averageOfLevels(TreeNode root) {
+        List < Integer > count = new ArrayList < > ();
+        List < Double > res = new ArrayList < > ();
+        average(root, 0, res, count);
+        for (int i = 0; i < res.size(); i++)
+            res.set(i, res.get(i) / count.get(i));
+        return res;
+    }
+    public void average(TreeNode t, int i, List < Double > sum, List < Integer > count) {
+        if (t == null)
+            return;
+        if (i < sum.size()) {
+            sum.set(i, sum.get(i) + t.val);
+            count.set(i, count.get(i) + 1);
+        } else {
+            sum.add(1.0 * t.val);
+            count.add(1);
+        }
+        average(t.left, i + 1, sum, count);
+        average(t.right, i + 1, sum, count);
+    }
+ */
+ /*
+ BFS
+ public List < Double > averageOfLevels(TreeNode root) {
+         List < Double > res = new ArrayList < > ();
+         Queue < TreeNode > queue = new LinkedList < > ();
+         queue.add(root);
+         while (!queue.isEmpty()) {
+             long sum = 0, count = 0;
+             Queue < TreeNode > temp = new LinkedList < > ();
+             while (!queue.isEmpty()) {
+                 TreeNode n = queue.remove();
+                 sum += n.val;
+                 count++;
+                 if (n.left != null)
+                     temp.add(n.left);
+                 if (n.right != null)
+                     temp.add(n.right);
+             }
+             queue = temp;
+             res.add(sum * 1.0 / count);
+         }
+         return res;
+     }
+  
+  */
