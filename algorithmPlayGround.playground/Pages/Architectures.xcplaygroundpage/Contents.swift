ef0667231f64cff5233c711022069f512c260d13
Sangchan <sangchan82@gmail.com>
2022-09-17 18:31:24 +0900

    add new

--- algorithmPlayGround.playground/Pages/Architectures.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/Architectures.xcplaygroundpage/Contents.swift
@@ -4,6 +4,50 @@ import Foundation
 
 // MVC
 
+class MVC_Model {
+    
+}
+
+class MVC_View {
+    
+}
+
+class MVC_Controller {
+    
+}
+
 // VIPER
 
+class VIPER_View {
+    
+}
+
+class VIPER_Interactor {
+    
+}
+
+class VIPER_Presenter {
+    
+}
+
+class VIPER_Entity {
+    
+}
+
+class VIPER_Router {
+    
+}
+
 // MVVM
+
+class MVVM_Model {
+    
+}
+
+class MVVM_View {
+    
+}
+
+class MVVM_ViewModel {
+    
+}
