2558b98d8d3f8cc8803fcc0676ad807b89d2ae4f
Sangchan <sangchan82@gmail.com>
2022-09-18 11:48:01 +0900

    add new

--- algorithmPlayGround.playground/Pages/Architectures.xcplaygroundpage/Contents.swift
+++ algorithmPlayGround.playground/Pages/Architectures.xcplaygroundpage/Contents.swift
@@ -5,15 +5,34 @@ import Foundation
 // MVC
 
 class MVC_Model {
+    var no : Int
+    var name : String
     
+    init(no : Int, name :  String) {
+        self.no = no
+        self.name = name
+    }
 }
 
 class MVC_View {
-    
+    func print(no : Int, name : String) {
+        Swift.print("no = \(no)")
+        Swift.print("name = \(name)")
+    }
 }
 
 class MVC_Controller {
+    var model : MVC_Model
+    var view : MVC_View
     
+    init(model : MVC_Model, view : MVC_View) {
+        self.model = model
+        self.view = view
+    }
+    
+    func update() {
+        view.print(no: model.no, name: model.name)
+    }
 }
 
 // VIPER
